69d240f6763006cca2293e33c4b703eee772119b
ale <criabiobr@gmail.com>
2023-07-20 20:44:44 +0100

    Testing DRF with Mocks

---
+++ test_post_api.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.utils import timezone
+
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+from blog.models import Post
+
+
+class PostApiTestCase(TestCase):
+    def setUp(self):
+        self.u1 = get_user_model().objects.create_user(
+            email="test@example.com", password="password"
+        )
+        self.u2 = get_user_model().objects.create_user(
+            email="test2@example.com", password="password2"
+        )
+        posts = [
+            Post.objects.create(
+                author=self.u1,
+                published_at=timezone.now(),
+                title="Post 1 Title",
+                slug="post-1-slug",
+                summary="Post 1 Summary",
+                content="Post 1 Content",
+            ),
+            Post.objects.create(
+                author=self.u2,
+                published_at=timezone.now(),
+                title="Post 2 Title",
+                slug="post-2-slug",
+                summary="Post 2 Summary",
+                content="Post 2 Content",
+            ),
+        ]
+        # let us look up the post info by ID
+        self.post_lookup = {p.id: p for p in posts}
+        # override test client
+        self.client = APIClient()
+        token = Token.objects.create(user=self.u1)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
+    def test_post_list(self):
+        resp = self.client.get("/api/v1/posts/")
+        data = resp.json()
+        self.assertEqual(len(data), 2)
+        for post_dict in data:
+            post_obj = self.post_lookup[post_dict["id"]]
+            self.assertEqual(post_obj.title, post_dict["title"])
+            self.assertEqual(post_obj.slug, post_dict["slug"])
+            self.assertEqual(post_obj.summary, post_dict["summary"])
+            self.assertEqual(post_obj.content, post_dict["content"])
+            # Modified the next line to check if the author is in the URL
+            self.assertTrue(post_dict["author"].endswith(f"/api/v1/users/{post_obj.author.email}"))
+            self.assertEqual(
+                post_obj.published_at,
+                datetime.strptime(
+                    post_dict["published_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                ).replace(tzinfo=timezone.utc)
+            )
