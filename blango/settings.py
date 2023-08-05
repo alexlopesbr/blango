52f05f21cc6513c482d6710a39b7182cbfc5c0e1
Alexandre Lopes <criabiobr@gmail.com>
2023-08-05 19:39:20 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from configurations import Configuration
 from configurations import values
+from datetime import timedelta
 import dj_database_url
 # import logging.config
 
@@ -21,11 +22,17 @@ import dj_database_url
 class Dev(Configuration):
   AUTH_USER_MODEL = "blango_auth.User"
 
+  SIMPLE_JWT = {
+      "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+      "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+  }
+
   REST_FRAMEWORK = {
       "DEFAULT_AUTHENTICATION_CLASSES": [
           "rest_framework.authentication.BasicAuthentication",
           "rest_framework.authentication.SessionAuthentication",
           "rest_framework.authentication.TokenAuthentication",
+          "rest_framework_simplejwt.authentication.JWTAuthentication",
       ],
 
       "DEFAULT_PERMISSION_CLASSES": [
