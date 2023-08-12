8fcdb81b3a1181a5cc64f10831d4b0754b760613
Alexandre Lopes <criabiobr@gmail.com>
2023-08-12 08:21:36 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,18 +1,49 @@
-function sayHello(yourName) {
-  if (yourName === undefined) {
-      console.log('Hello, no name')
-  } else {
-       console.log('Hello, ' + yourName)
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
+
+  getGreeting () {
+    if (this.name === undefined) {
+      return 'Hello, no name'
+    }
+
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage) {
+    console.log(greetingMessage)
+  }
+
+  greet () {
+    this.showGreeting(this.getGreeting())
   }
 }
 
-const yourName = 'Alex'  // Put your name here
+class DelayedGreeter extends Greeter {
+  delay = 2000
 
-console.log('Before setTimeout')
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
 
-setTimeout(() => {
-    sayHello(yourName)
-  }, 2000
-)
+  greet () {
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
+}
 
-console.log('After setTimeout')
+const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+dg2.greet()
+
+const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+dg1.greet()
+
+const g = new Greeter('Alex')  // Put your name here if you like
+g.greet()
